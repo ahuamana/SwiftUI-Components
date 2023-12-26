f01de10f1e6eb745fa142428f1199ec591e5bf9a
ahuamana <73962071@continental.edu.pe>
2023-12-26 07:58:47 -0500

    Initial Commit

---
+++ Platzi/PlatziApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatziApp.swift
+//  Platzi
+//
+//  Created by Antony Huaman Alikhan on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PlatziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
